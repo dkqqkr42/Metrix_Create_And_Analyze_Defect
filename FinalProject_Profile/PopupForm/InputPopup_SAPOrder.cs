80636ead0a5f4e390c144801f47491414ed3a8ca
Shin <hacker961011@gmail.com>
2020-11-04 16:27:19 +0900

    수정중...

--- FinalProject_Profile/PopupForm/InputPopup_SAPOrder.cs
+++ FinalProject_Profile/PopupForm/InputPopup_SAPOrder.cs
@@ -8,14 +8,157 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MetroFramework.Forms;
+using Oracle.ManagedDataAccess.Client;
 
 namespace FinalProject_Profile.PopupForm
 {
     public partial class InputPopup_SAPOrder : MetroForm
     {
+        protected const string connectionString = "DATA SOURCE=220.69.249.228:1521/xe;PASSWORD=1234;PERSIST SECURITY INFO=True;USER ID=MAT_MGR";
+
         public InputPopup_SAPOrder()
         {
             InitializeComponent();
+        }
+
+        /*
+        public InputPopup_SAPOrder(List<string> list)
+        {
+            InitializeComponent();
+            txt_PLANT_CODE.Text = list[1];
+            txt_MRP_MGR.Text = list[2];
+            txt_ORDER_NO.Text = list[3];
+            txt_ORDER_SEQ.Text = list[4];
+            txt_ORDER_DATE.Text = list[5];
+            txt_PROD_CODE.Text = list[6];
+            txt_PROD_UNIT.Text = list[7];
+            txt_P_VERSION.Text = list[8];
+            txt_WC_CODE.Text = list[9];
+            txt_DUE_DATE_A.Text = list[10];
+            txt_DUE_DATE_B.Text = list[11];
+            txt_ORDER_QTY.Text = list[12];
+            txt_ROLL_METER.Text = list[13];
+            txt_ORDER_UNIT_PRICE.Text = list[14];
+            txt_S_LOCATION.Text = list[15];
+            txt_EXCHG_RATE.Text = list[16];
+            txt_CURRENCY_UNIT.Text = list[17];
+            txt_PROD_TYPE.Text = list[18];
+            txt_CUST_CODE.Text = list[19];
+            txt_CUST_NAME.Text = list[20];
+            txt_DISTRB_CHL.Text = list[21];
+            txt_ORDER_TYPE.Text = list[22];
+            txt_CONFIRM_FLAG.Text = list[23];
+            txt_REMARK.Text = list[24];
+            txt_COMPLETE_FLAG.Text = list[25];
+            txt_PLAN_QTY.Text = list[26];
+            txt_INPUT_QTY.Text = list[27];
+            txt_WORK_TIME.Text = list[28];
+            txt_INSERT_DATE.Text = list[29];
+        }
+        */
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            OracleConnection connection = null;
+            try
+            {
+                connection = new OracleConnection
+                {
+                    ConnectionString = connectionString
+                };
+                connection.Open();
+
+                OracleCommand cmd = new OracleCommand
+                {
+                    CommandType = CommandType.StoredProcedure,
+                    Connection = connection,
+                    CommandText = "insert into tbl_saporder values(:IN_PLANT_CODE, :IN_MRP_MGR, :IN_ORDER_NO, :IN_ORDER_SEQ, :IN_ORDER_DATE, :IN_PROD_CODE, :IN_PROD_UNIT, :IN_P_VERSION, :IN_WC_CODE, :IN_DUE_DATE_A, :IN_DUE_DATE_B, :IN_ORDER_QTY, :IN_ROLL_METER, :IN_ORDER_UNIT_PRICE, :IN_S_LOCATION, :IN_EXCHG_RATE, :IN_CURRENCY_UNIT, :IN_PROD_TYPE, :IN_CUST_CODE, :IN_CUST_NAME, :IN_DISTRB_CHL, :IN_ORDER_TYPE, :IN_CONFIRM_FLAG, :IN_REMARK, :IN_COMPLETE_FLAG, :IN_PLAN_QTY, :IN_INPUT_QTY, :IN_WORK_TIME, :IN_INSERT_DATE)",
+                    BindByName =true
+                };
+
+                // cmd.CommandText = "insert into tbl_saporder values(:IN_PLANT_CODE, :IN_MRP_MGR, :IN_ORDER_NO, :IN_ORDER_SEQ, :IN_ORDER_DATE, :IN_PROD_CODE, :IN_PROD_UNIT, :IN_P_VERSION, :IN_WC_CODE, :IN_DUE_DATE_A, :IN_DUE_DATE_B)";
+
+                /*
+                cmd.Parameters.Add(new OracleParameter("IN_PLANT_CODE", txt_PLANT_CODE.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_MRP_MGR", txt_MRP_MGR.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_ORDER_NO", txt_ORDER_NO.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_ORDER_SEQ", txt_ORDER_SEQ.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_ORDER_DATE", txt_ORDER_DATE.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_PROD_CODE", txt_PROD_CODE.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_PROD_UNIT", txt_PROD_UNIT.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_P_VERSION", txt_P_VERSION.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_WC_CODE", txt_WC_CODE.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_DUE_DATE_A", txt_DUE_DATE_A.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_DUE_DATE_B", txt_DUE_DATE_B.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_ORDER_QTY", txt_ORDER_QTY.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_ROLL_METER", txt_ROLL_METER.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_ORDER_UNIT_PRICE", txt_ORDER_UNIT_PRICE.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_S_LOCATION", txt_S_LOCATION.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_EXCHG_RATE", txt_EXCHG_RATE.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_CURRENCY_UNIT", txt_CURRENCY_UNIT.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_PROD_TYPE", txt_PROD_TYPE.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_CUST_CODE", txt_CUST_CODE.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_CUST_NAME", txt_CUST_NAME.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_DISTRB_CHL", txt_DISTRB_CHL.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_ORDER_TYPE", txt_ORDER_TYPE.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_CONFIRM_FLAG", txt_CONFIRM_FLAG.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_REMARK", txt_REMARK.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_COMPLETE_FLAG", txt_COMPLETE_FLAG.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_PLAN_QTY", txt_PLAN_QTY.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_INPUT_QTY", txt_INPUT_QTY.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_WORK_TIME", txt_WORK_TIME.Text));
+                cmd.Parameters.Add(new OracleParameter("IN_INSERT_DATE", DateTime.Now));
+                */
+                
+                cmd.Parameters.Add("IN_PLANT_CODE", txt_PLANT_CODE.Text);
+                cmd.Parameters.Add("IN_MRP_MGR", txt_MRP_MGR.Text);
+                cmd.Parameters.Add("IN_ORDER_NO", txt_ORDER_NO.Text);
+                cmd.Parameters.Add("IN_ORDER_SEQ", txt_ORDER_SEQ.Text);
+                cmd.Parameters.Add("IN_ORDER_DATE", txt_ORDER_DATE.Text);
+                cmd.Parameters.Add("IN_PROD_CODE", txt_PROD_CODE.Text);
+                cmd.Parameters.Add("IN_PROD_UNIT", txt_PROD_UNIT.Text);
+                cmd.Parameters.Add("IN_P_VERSION", txt_P_VERSION.Text);
+                cmd.Parameters.Add("IN_WC_CODE", txt_WC_CODE.Text);
+                cmd.Parameters.Add("IN_DUE_DATE_A", txt_DUE_DATE_A.Text);
+                cmd.Parameters.Add("IN_DUE_DATE_B", txt_DUE_DATE_B.Text);
+                cmd.Parameters.Add("IN_ORDER_QTY", txt_ORDER_QTY.Text);
+                cmd.Parameters.Add("IN_ROLL_METER", txt_ROLL_METER.Text);
+                cmd.Parameters.Add("IN_ORDER_UNIT_PRICE", txt_ORDER_UNIT_PRICE.Text);
+                cmd.Parameters.Add("IN_S_LOCATION", txt_S_LOCATION.Text);
+                cmd.Parameters.Add("IN_EXCHG_RATE", txt_EXCHG_RATE.Text);
+                cmd.Parameters.Add("IN_CURRENCY_UNIT", txt_CURRENCY_UNIT.Text);
+                cmd.Parameters.Add("IN_PROD_TYPE", txt_PROD_TYPE.Text);
+                cmd.Parameters.Add("IN_CUST_CODE", txt_CUST_CODE.Text);
+                cmd.Parameters.Add("IN_CUST_NAME", txt_CUST_NAME.Text);
+                cmd.Parameters.Add("IN_DISTRB_CHL", txt_DISTRB_CHL.Text);
+                cmd.Parameters.Add("IN_ORDER_TYPE", txt_ORDER_TYPE.Text);
+                cmd.Parameters.Add("IN_CONFIRM_FLAG", txt_CONFIRM_FLAG.Text);
+                cmd.Parameters.Add("IN_REMARK", txt_REMARK.Text);
+                cmd.Parameters.Add("IN_COMPLETE_FLAG", txt_COMPLETE_FLAG.Text);
+                cmd.Parameters.Add("IN_PLAN_QTY", txt_PLAN_QTY.Text);
+                cmd.Parameters.Add("IN_INPUT_QTY", txt_INPUT_QTY.Text);
+                cmd.Parameters.Add("IN_WORK_TIME", txt_WORK_TIME.Text);
+                cmd.Parameters.Add("IN_INSERT_DATE", DateTime.Now);
+                
+
+
+                cmd.ExecuteNonQuery();
+
+                OracleDataReader dr = cmd.ExecuteReader();
+
+                // get the data
+                dr.Read();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            
         }
 
         private void btn_Save_MouseUp(object sender, MouseEventArgs e)
--- FinalProject_Profile/SAPOrder.cs
+++ FinalProject_Profile/SAPOrder.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using FinalProject_Profile.PopupForm;
 using MetroFramework.Forms;
 using Oracle.ManagedDataAccess.Client;
@@ -15,10 +16,81 @@ namespace FinalProject_Profile
     public partial class SAPOrder : MetroForm
     {
         protected const string connectionString = "DATA SOURCE=220.69.249.228:1521/xe;PASSWORD=1234;PERSIST SECURITY INFO=True;USER ID=MAT_MGR";
+        InputPopup_SAPOrder popup;
+        List<string> list = new List<string>();
 
         public SAPOrder()
         {
             InitializeComponent();
+            cbo_TEAM.SelectedIndex = 0;
+            cbo_WORKCENTER.SelectedIndex = 0;
+        }
+
+        public void FillGrid(DataSet ds)  // 데이터 조회 결과를 adapter로 받아 DataSet을 반환 받은 경우 사용되는 메서드
+        {
+            grd_Result.DataSource = ds.Tables[0];
+        }
+
+        public int SelectItem()
+        {
+            OracleConnection connection = null;
+            try
+            {
+                connection = new OracleConnection
+                {
+                    ConnectionString = connectionString
+                };
+                connection.Open();
+
+                OracleCommand cmd = new OracleCommand
+                {
+                    CommandType = CommandType.Text,
+                    Connection = connection,
+                    CommandText = "SELECT trim(TBL_SAPORDER.PLANT_CODE), trim(TBL_SAPORDER.ORDER_NO), trim(TBL_SAPORDER.ORDER_SEQ), trim(TBL_SAPORDER.ORDER_DATE), trim(TBL_SAPORDER.PROD_CODE), trim(TBL_SAPORDER.PROD_UNIT), trim(TBL_SAPORDER.P_VERSION), trim(TBL_SAPORDER.WC_CODE), trim(TBL_SAPORDER.DUE_DATE_A), trim(TBL_SAPORDER.DUE_DATE_B), trim(TBL_SAPORDER.ORDER_QTY), trim(TBL_SAPORDER.ROLL_METER), trim(TBL_SAPORDER.ORDER_UNIT_PRICE), trim(TBL_SAPORDER.S_LOCATION), trim(TBL_SAPORDER.EXCHG_RATE), trim(TBL_SAPORDER.CURRENCY_UNIT), trim(TBL_SAPORDER.PROD_TYPE), trim(TBL_SAPORDER.MRP_MGR), trim(TBL_SAPORDER.CUST_CODE), trim(TBL_SAPORDER.CUST_NAME), trim(TBL_SAPORDER.DISTRB_CHL), trim(TBL_SAPORDER.ORDER_TYPE), trim(TBL_SAPORDER.CONFIRM_FLAG), trim(TBL_SAPORDER.REMARK), trim(TBL_SAPORDER.PLAN_QTY), trim(TBL_SAPORDER.INPUT_QTY), trim(TBL_SAPORDER.WORK_TIME), trim(TBL_PRODUCTMASTER.PROD_NAME), trim(TBL_WORKCENTER.WC_NAME), trim(TBL_WORKCENTER.REQ_MP_MAN), trim(TBL_SAPORDER.COMPLETE_FLAG), trim(TBL_WORKCENTER.GONGBU_DIV) FROM TBL_SAPORDER, TBL_PRODUCTMASTER, TBL_WORKCENTER WHERE TBL_SAPORDER.PLANT_CODE = TBL_WORKCENTER.PLANT_CODE AND  TBL_SAPORDER.WC_CODE = TBL_WORKCENTER.WC_CODE AND  TBL_SAPORDER.MRP_MGR = 'F63' AND  TBL_SAPORDER.PROD_CODE = TBL_PRODUCTMASTER.PROD_CODE"
+                };
+
+                /*
+                // 생산팀 콤보박스 값에 따른 조회
+                if (cbo_TEAM.Text.Equals("전체"))
+                    cmd.CommandText = "SELECT trim(TBL_SAPORDER.PLANT_CODE), trim(TBL_SAPORDER.ORDER_NO), trim(TBL_SAPORDER.ORDER_SEQ), trim(TBL_SAPORDER.ORDER_DATE), trim(TBL_SAPORDER.PROD_CODE), trim(TBL_SAPORDER.PROD_UNIT), trim(TBL_SAPORDER.P_VERSION), trim(TBL_SAPORDER.WC_CODE), trim(TBL_SAPORDER.DUE_DATE_A), trim(TBL_SAPORDER.DUE_DATE_B), trim(TBL_SAPORDER.ORDER_QTY), trim(TBL_SAPORDER.ROLL_METER), trim(TBL_SAPORDER.ORDER_UNIT_PRICE), trim(TBL_SAPORDER.S_LOCATION), trim(TBL_SAPORDER.EXCHG_RATE), trim(TBL_SAPORDER.CURRENCY_UNIT), trim(TBL_SAPORDER.PROD_TYPE), trim(TBL_SAPORDER.MRP_MGR), trim(TBL_SAPORDER.CUST_CODE), trim(TBL_SAPORDER.CUST_NAME), trim(TBL_SAPORDER.DISTRB_CHL), trim(TBL_SAPORDER.ORDER_TYPE), trim(TBL_SAPORDER.CONFIRM_FLAG), trim(TBL_SAPORDER.REMARK), trim(TBL_SAPORDER.PLAN_QTY), trim(TBL_SAPORDER.INPUT_QTY), trim(TBL_SAPORDER.WORK_TIME), trim(TBL_PRODUCTMASTER.PROD_NAME), trim(TBL_WORKCENTER.WC_NAME), trim(TBL_WORKCENTER.REQ_MP_MAN), trim(TBL_SAPORDER.COMPLETE_FLAG), trim(TBL_WORKCENTER.GONGBU_DIV) FROM TBL_SAPORDER, TBL_PRODUCTMASTER, TBL_WORKCENTER WHERE TBL_SAPORDER.PLANT_CODE = TBL_WORKCENTER.PLANT_CODE AND  TBL_SAPORDER.WC_CODE = TBL_WORKCENTER.WC_CODE AND  TBL_SAPORDER.MRP_MGR = 'F63' AND  TBL_SAPORDER.PROD_CODE = TBL_PRODUCTMASTER.PROD_CODE";
+
+                else if (cbo_TEAM.Text.Equals("카매트생산팀"))
+                {
+                    if (cbo_WORKCENTER.Equals("전체"))
+                        cmd.CommandText = "SELECT trim(TBL_SAPORDER.PLANT_CODE), trim(TBL_SAPORDER.ORDER_NO), trim(TBL_SAPORDER.ORDER_SEQ), trim(TBL_SAPORDER.ORDER_DATE), trim(TBL_SAPORDER.PROD_CODE), trim(TBL_SAPORDER.PROD_UNIT), trim(TBL_SAPORDER.P_VERSION), trim(TBL_SAPORDER.WC_CODE), trim(TBL_SAPORDER.DUE_DATE_A), trim(TBL_SAPORDER.DUE_DATE_B), trim(TBL_SAPORDER.ORDER_QTY), trim(TBL_SAPORDER.ROLL_METER), trim(TBL_SAPORDER.ORDER_UNIT_PRICE), trim(TBL_SAPORDER.S_LOCATION), trim(TBL_SAPORDER.EXCHG_RATE), trim(TBL_SAPORDER.CURRENCY_UNIT), trim(TBL_SAPORDER.PROD_TYPE), trim(TBL_SAPORDER.MRP_MGR), trim(TBL_SAPORDER.CUST_CODE), trim(TBL_SAPORDER.CUST_NAME), trim(TBL_SAPORDER.DISTRB_CHL), trim(TBL_SAPORDER.ORDER_TYPE), trim(TBL_SAPORDER.CONFIRM_FLAG), trim(TBL_SAPORDER.REMARK), trim(TBL_SAPORDER.PLAN_QTY), trim(TBL_SAPORDER.INPUT_QTY), trim(TBL_SAPORDER.WORK_TIME), trim(TBL_PRODUCTMASTER.PROD_NAME), trim(TBL_WORKCENTER.WC_NAME), trim(TBL_WORKCENTER.REQ_MP_MAN), trim(TBL_SAPORDER.COMPLETE_FLAG), trim(TBL_WORKCENTER.GONGBU_DIV) FROM TBL_SAPORDER, TBL_PRODUCTMASTER, TBL_WORKCENTER WHERE TBL_SAPORDER.PLANT_CODE = TBL_WORKCENTER.PLANT_CODE AND  TBL_SAPORDER.WC_CODE = TBL_WORKCENTER.WC_CODE AND  TBL_SAPORDER.MRP_MGR = 'F63' AND  TBL_SAPORDER.PROD_CODE = TBL_PRODUCTMASTER.PROD_CODE";
+
+                    else if (cbo_WORKCENTER.Equals("1호카매트"))
+                        cmd.CommandText = "SELECT trim(TBL_SAPORDER.PLANT_CODE), trim(TBL_SAPORDER.ORDER_NO), trim(TBL_SAPORDER.ORDER_SEQ), trim(TBL_SAPORDER.ORDER_DATE), trim(TBL_SAPORDER.PROD_CODE), trim(TBL_SAPORDER.PROD_UNIT), trim(TBL_SAPORDER.P_VERSION), trim(TBL_SAPORDER.WC_CODE), trim(TBL_SAPORDER.DUE_DATE_A), trim(TBL_SAPORDER.DUE_DATE_B), trim(TBL_SAPORDER.ORDER_QTY), trim(TBL_SAPORDER.ROLL_METER), trim(TBL_SAPORDER.ORDER_UNIT_PRICE), trim(TBL_SAPORDER.S_LOCATION), trim(TBL_SAPORDER.EXCHG_RATE), trim(TBL_SAPORDER.CURRENCY_UNIT), trim(TBL_SAPORDER.PROD_TYPE), trim(TBL_SAPORDER.MRP_MGR), trim(TBL_SAPORDER.CUST_CODE), trim(TBL_SAPORDER.CUST_NAME), trim(TBL_SAPORDER.DISTRB_CHL), trim(TBL_SAPORDER.ORDER_TYPE), trim(TBL_SAPORDER.CONFIRM_FLAG), trim(TBL_SAPORDER.REMARK), trim(TBL_SAPORDER.PLAN_QTY), trim(TBL_SAPORDER.INPUT_QTY), trim(TBL_SAPORDER.WORK_TIME), trim(TBL_PRODUCTMASTER.PROD_NAME), trim(TBL_WORKCENTER.WC_NAME), trim(TBL_WORKCENTER.REQ_MP_MAN), trim(TBL_SAPORDER.COMPLETE_FLAG), trim(TBL_WORKCENTER.GONGBU_DIV) FROM TBL_SAPORDER, TBL_PRODUCTMASTER, TBL_WORKCENTER WHERE TBL_SAPORDER.PLANT_CODE = TBL_WORKCENTER.PLANT_CODE AND  TBL_SAPORDER.WC_CODE = TBL_WORKCENTER.WC_CODE AND  TBL_SAPORDER.MRP_MGR = 'F63' AND  TBL_SAPORDER.PROD_CODE = TBL_PRODUCTMASTER.PROD_CODE AND TBL_WORKCENTER.WC_NAME = '1호카매트'";
+
+                    else if (cbo_WORKCENTER.Equals("2호카매트"))
+                        cmd.CommandText = "SELECT trim(TBL_SAPORDER.PLANT_CODE), trim(TBL_SAPORDER.ORDER_NO), trim(TBL_SAPORDER.ORDER_SEQ), trim(TBL_SAPORDER.ORDER_DATE), trim(TBL_SAPORDER.PROD_CODE), trim(TBL_SAPORDER.PROD_UNIT), trim(TBL_SAPORDER.P_VERSION), trim(TBL_SAPORDER.WC_CODE), trim(TBL_SAPORDER.DUE_DATE_A), trim(TBL_SAPORDER.DUE_DATE_B), trim(TBL_SAPORDER.ORDER_QTY), trim(TBL_SAPORDER.ROLL_METER), trim(TBL_SAPORDER.ORDER_UNIT_PRICE), trim(TBL_SAPORDER.S_LOCATION), trim(TBL_SAPORDER.EXCHG_RATE), trim(TBL_SAPORDER.CURRENCY_UNIT), trim(TBL_SAPORDER.PROD_TYPE), trim(TBL_SAPORDER.MRP_MGR), trim(TBL_SAPORDER.CUST_CODE), trim(TBL_SAPORDER.CUST_NAME), trim(TBL_SAPORDER.DISTRB_CHL), trim(TBL_SAPORDER.ORDER_TYPE), trim(TBL_SAPORDER.CONFIRM_FLAG), trim(TBL_SAPORDER.REMARK), trim(TBL_SAPORDER.PLAN_QTY), trim(TBL_SAPORDER.INPUT_QTY), trim(TBL_SAPORDER.WORK_TIME), trim(TBL_PRODUCTMASTER.PROD_NAME), trim(TBL_WORKCENTER.WC_NAME), trim(TBL_WORKCENTER.REQ_MP_MAN), trim(TBL_SAPORDER.COMPLETE_FLAG), trim(TBL_WORKCENTER.GONGBU_DIV) FROM TBL_SAPORDER, TBL_PRODUCTMASTER, TBL_WORKCENTER WHERE TBL_SAPORDER.PLANT_CODE = TBL_WORKCENTER.PLANT_CODE AND  TBL_SAPORDER.WC_CODE = TBL_WORKCENTER.WC_CODE AND  TBL_SAPORDER.MRP_MGR = 'F63' AND  TBL_SAPORDER.PROD_CODE = TBL_PRODUCTMASTER.PROD_CODE AND TBL_WORKCENTER.WC_NAME = '2호카매트'";
+                }
+                */
+
+                OracleDataReader reader = cmd.ExecuteReader();
+
+                grd_Result.Rows.Clear();
+
+                int i_cnt = 0;
+
+                while (reader.Read())
+                {
+                    grd_Result.Rows.Add(++i_cnt, reader[28], reader[1], reader[3], reader[8], reader[20], reader[4], reader[27], reader[5], reader[10], reader[30]);
+                }
+
+                return i_cnt;
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return 0;
+            }
+            finally
+            {
+                connection.Close();
+            }
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -39,7 +111,27 @@ namespace FinalProject_Profile
 
         private void metroButton3_Click(object sender, EventArgs e)  // 조회버튼 누를 경우 실행
         {
+            SelectItem();
+        }
 
+        private void metroButton3_MouseUp(object sender, MouseEventArgs e)
+        {
+            btn_Select.BackgroundImage = FinalProject_Profile.Properties.Resources.조회;
+        }
+
+        private void metroButton3_MouseDown(object sender, MouseEventArgs e)
+        {
+            btn_Select.BackgroundImage = FinalProject_Profile.Properties.Resources.조회클릭;
+        }
+
+        private void metroButton2_MouseUp(object sender, MouseEventArgs e)
+        {
+            btn_Insert.BackgroundImage = FinalProject_Profile.Properties.Resources.입력;
+        }
+
+        private void metroButton2_MouseDown(object sender, MouseEventArgs e)
+        {
+            btn_Insert.BackgroundImage = FinalProject_Profile.Properties.Resources.입력클릭;
         }
     }
 }
