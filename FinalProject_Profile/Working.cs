2c2df9b3d393b7ed664d1f44f1bd40e192578ce4
dkqqkr42 <dkqqkr42@naver.com>
2020-11-10 14:38:03 +0900

    이름 변경

--- FinalProject_Profile/Working.cs
+++ FinalProject_Profile/Working.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MetroFramework.Forms;
+using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,36 +8,52 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
-using MetroFramework.Controls;
-using MetroFramework.Forms;
 using System.Timers;
-using Oracle.ManagedDataAccess.Client;
+using System.Windows.Forms;
 
 namespace FinalProject_Profile
 {
     public partial class Working : MetroForm
     {
         int box_pcs, plt_box, cut_pcs;
-        string prod_code, job_no, prod_name, prod_unit, order_m, work_gbn, gubun;
-        int pcs=0, box=0, plt=0;
+        string prod_code, order_no, job_no, prod_name, prod_unit, order_m, work_gbn, gubun;
+        int pcs = 0, box = 0, plt = 0;
+
+        private void btn_InspectionStart_Click(object sender, EventArgs e)
+        {
+            StartTimer();
+        }
+
+        private void Working2_Load(object sender, EventArgs e)
+        {
+            SelectItem();
+        }
 
         protected const string connectionString = "DATA SOURCE=220.69.249.228:1521/xe;PASSWORD=1234;PERSIST SECURITY INFO=True;USER ID=MAT_MGR";
         public Working()
         {
             InitializeComponent();
-            for (int i = 0; i < 1; i++)
+        }
+        protected override CreateParams CreateParams
+        {
+            get
             {
-                metroGrid1.Rows.Add("1", "1234", "2020/09/20", "2020/09/20", "내수");
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x02000000;
+                return cp;
             }
-            metroTile6.Font = new Font(metroTile6.Font.FontFamily, 50);
         }
 
-        private void Wokring_Load(object sender, EventArgs e)
+        private void btn_InspectionStart_MouseUp(object sender, MouseEventArgs e)
         {
-            SelectItem();
-            StartTimer();
+            btn_InspectionStart.BackgroundImage = FinalProject_Profile.Properties.Resources.검사시작;
         }
+
+        private void btn_InspectionStart_MouseDown(object sender, MouseEventArgs e)
+        {
+            btn_InspectionStart.BackgroundImage = FinalProject_Profile.Properties.Resources.검사시작클릭;
+        }
+
         public void SelectItem()
         {
             OracleConnection connection = null;
@@ -52,7 +70,7 @@ namespace FinalProject_Profile
                 {
                     CommandType = CommandType.Text,
                     Connection = connection,
-                    CommandText = "SELECT * FROM (select A.JOB_NO JOB_NO, RESERVE_RANK, A.PLANT_CODE PLANT_CODE, A.ORDER_NO||' - '||A.ORDER_SEQ ORDER_NO, A.ORDER_M ORDER_M, A.ADD_GOOD_QTY ADD_GOOD_QTY, DECODE(A.GUBUN, 20, '내수', 40, '수출', 10, '특판') GUBUN, DECODE(A.WORK_GUBUN, 'A', '정상작업', 'U', '재작업', 'R', '연구개발', 'T', '기술테스트', 'S', 'S/BOOK', 'C', 'C/MATCH') WORK_GBN, DECODE(A.NOTE_FLAG, 'Y', '★') NOTE_FLAG, A.WORK_GUBUN WORK_GUBUN, A.WC_CODE WC_CODE, trim(A.PROD_CODE) PROD_CODE, PROD_NAME, A.PROD_UNIT PROD_UNIT, A.MRP_MGR MRP_MGR, NOTE0, NOTE1, TO_CHAR(START_DATE, 'HH24:MI') SDATE, TO_CHAR(END_DATE, 'HH24:MI') EDATE FROM TBL_PRODUCTPLAN A, TBL_PRODUCTMASTER B, TBL_PRODRESERVE C WHERE A.PROD_CODE  = B.PROD_CODE AND A.WC_CODE = C.WC_CODE AND A.JOB_NO = C.JOB_NO AND A.PLANT_CODE = '2020' AND A.PROC_STATUS = 'A' AND A.DEL_FLAG = 'A' ORDER BY RESERVE_RANK) WHERE ROWNUM = 1"
+                    CommandText = "SELECT * FROM (select A.JOB_NO JOB_NO, RESERVE_RANK, A.PLANT_CODE PLANT_CODE, trim(A.ORDER_NO||' - '||A.ORDER_SEQ) ORDER_NO, A.ORDER_M ORDER_M, A.ADD_GOOD_QTY ADD_GOOD_QTY, DECODE(A.GUBUN, 20, '내수', 40, '수출', 10, '특판') GUBUN, DECODE(A.WORK_GUBUN, 'A', '정상작업', 'U', '재작업', 'R', '연구개발', 'T', '기술테스트', 'S', 'S/BOOK', 'C', 'C/MATCH') WORK_GBN, DECODE(A.NOTE_FLAG, 'Y', '★') NOTE_FLAG, A.WORK_GUBUN WORK_GUBUN, A.WC_CODE WC_CODE, trim(A.PROD_CODE) PROD_CODE, PROD_NAME, A.PROD_UNIT PROD_UNIT, A.MRP_MGR MRP_MGR, NOTE0, NOTE1, TO_CHAR(START_DATE, 'HH24:MI') SDATE, TO_CHAR(END_DATE, 'HH24:MI') EDATE FROM TBL_PRODUCTPLAN A, TBL_PRODUCTMASTER B, TBL_PRODRESERVE C WHERE A.PROD_CODE  = B.PROD_CODE AND A.WC_CODE = C.WC_CODE AND A.JOB_NO = C.JOB_NO AND A.PLANT_CODE = '2020' AND A.PROC_STATUS = 'A' AND A.DEL_FLAG = 'A' ORDER BY RESERVE_RANK) WHERE ROWNUM = 1"
                 };
 
 
@@ -64,6 +82,7 @@ namespace FinalProject_Profile
                 reader.Read();
 
                 prod_code = reader["PROD_CODE"].ToString();
+                order_no = reader["ORDER_NO"].ToString();
                 job_no = reader["JOB_NO"].ToString();
                 prod_name = reader["PROD_NAME"].ToString();
                 prod_unit = reader["PROD_UNIT"].ToString();
@@ -89,7 +108,16 @@ namespace FinalProject_Profile
                 plt_box = Int32.Parse(reader["PLTBOX"].ToString());
                 cut_pcs = Int32.Parse(reader["CUTPCS"].ToString());
 
-                //todo 생산테이블에서 데이터 조회 후 데이터를 컨트롤에 집어 넣는 작업
+                //생산테이블에서 데이터 조회 후 데이터를 컨트롤에 집어 넣는 작업
+                lbl_PROD_CODE.Text = prod_code;
+                lbl_ORDER_NO.Text = order_no;
+                lbl_PROD_NAME.Text = prod_name;
+                lbl_PROD_UNIT.Text = prod_unit;
+                lbl_ORDER_M.Text = order_m;
+                lbl_WORK_GBN.Text = work_gbn;
+                lbl_GUBUN.Text = gubun;
+
+
 
                 //현재 진행중인 작업을 생산진행중 으로 바꾸는 쿼리
                 cmd = new OracleCommand
@@ -119,7 +147,7 @@ namespace FinalProject_Profile
         {
             System.Timers.Timer timer = new System.Timers.Timer
             {
-                Interval = 100,
+                Interval = 1000,
             };
             timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
             timer.Start();
@@ -141,7 +169,7 @@ namespace FinalProject_Profile
         }
         public void UpdateData()
         {
-            if (lbl_PCS.InvokeRequired || lbl_BOX.InvokeRequired || lbl_PLT.InvokeRequired)
+            if (lbl_Prodution.InvokeRequired || lbl_BOX.InvokeRequired || lbl_PLT.InvokeRequired)
             {
                 Invoke((MethodInvoker)delegate () {
                     UpdateData();
@@ -149,12 +177,10 @@ namespace FinalProject_Profile
             }
             else
             {
-                lbl_PCS.Text = pcs.ToString();
+                lbl_Prodution.Text = pcs.ToString();
                 lbl_BOX.Text = box.ToString();
                 lbl_PLT.Text = plt.ToString();
             }
-
-            
         }
     }
 }
--- FinalProject_Profile/_Working.cs
+++ FinalProject_Profile/_Working.cs
@@ -1,6 +1,4 @@
-﻿using MetroFramework.Forms;
-using Oracle.ManagedDataAccess.Client;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,48 +6,36 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Timers;
 using System.Windows.Forms;
+using MetroFramework.Controls;
+using MetroFramework.Forms;
+using System.Timers;
+using Oracle.ManagedDataAccess.Client;
 
 namespace FinalProject_Profile
 {
-    public partial class Working2 : MetroForm
+    public partial class _Working : MetroForm
     {
         int box_pcs, plt_box, cut_pcs;
-        string prod_code, order_no, job_no, prod_name, prod_unit, order_m, work_gbn, gubun;
-        int pcs = 0, box = 0, plt = 0;
+        string prod_code, job_no, prod_name, prod_unit, order_m, work_gbn, gubun;
+        int pcs=0, box=0, plt=0;
 
-        private void Working2_Load(object sender, EventArgs e)
+        protected const string connectionString = "DATA SOURCE=220.69.249.228:1521/xe;PASSWORD=1234;PERSIST SECURITY INFO=True;USER ID=MAT_MGR";
+        public _Working()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 1; i++)
+            {
+                metroGrid1.Rows.Add("1", "1234", "2020/09/20", "2020/09/20", "내수");
+            }
+            metroTile6.Font = new Font(metroTile6.Font.FontFamily, 50);
+        }
+
+        private void Wokring_Load(object sender, EventArgs e)
         {
             SelectItem();
             StartTimer();
         }
-
-        protected const string connectionString = "DATA SOURCE=220.69.249.228:1521/xe;PASSWORD=1234;PERSIST SECURITY INFO=True;USER ID=MAT_MGR";
-        public Working2()
-        {
-            InitializeComponent();
-        }
-        protected override CreateParams CreateParams
-        {
-            get
-            {
-                CreateParams cp = base.CreateParams;
-                cp.ExStyle |= 0x02000000;
-                return cp;
-            }
-        }
-
-        private void btn_InspectionStart_MouseUp(object sender, MouseEventArgs e)
-        {
-            btn_InspectionStart.BackgroundImage = FinalProject_Profile.Properties.Resources.검사시작;
-        }
-
-        private void btn_InspectionStart_MouseDown(object sender, MouseEventArgs e)
-        {
-            btn_InspectionStart.BackgroundImage = FinalProject_Profile.Properties.Resources.검사시작클릭;
-        }
-
         public void SelectItem()
         {
             OracleConnection connection = null;
@@ -66,7 +52,7 @@ namespace FinalProject_Profile
                 {
                     CommandType = CommandType.Text,
                     Connection = connection,
-                    CommandText = "SELECT * FROM (select A.JOB_NO JOB_NO, RESERVE_RANK, A.PLANT_CODE PLANT_CODE, trim(A.ORDER_NO||' - '||A.ORDER_SEQ) ORDER_NO, A.ORDER_M ORDER_M, A.ADD_GOOD_QTY ADD_GOOD_QTY, DECODE(A.GUBUN, 20, '내수', 40, '수출', 10, '특판') GUBUN, DECODE(A.WORK_GUBUN, 'A', '정상작업', 'U', '재작업', 'R', '연구개발', 'T', '기술테스트', 'S', 'S/BOOK', 'C', 'C/MATCH') WORK_GBN, DECODE(A.NOTE_FLAG, 'Y', '★') NOTE_FLAG, A.WORK_GUBUN WORK_GUBUN, A.WC_CODE WC_CODE, trim(A.PROD_CODE) PROD_CODE, PROD_NAME, A.PROD_UNIT PROD_UNIT, A.MRP_MGR MRP_MGR, NOTE0, NOTE1, TO_CHAR(START_DATE, 'HH24:MI') SDATE, TO_CHAR(END_DATE, 'HH24:MI') EDATE FROM TBL_PRODUCTPLAN A, TBL_PRODUCTMASTER B, TBL_PRODRESERVE C WHERE A.PROD_CODE  = B.PROD_CODE AND A.WC_CODE = C.WC_CODE AND A.JOB_NO = C.JOB_NO AND A.PLANT_CODE = '2020' AND A.PROC_STATUS = 'A' AND A.DEL_FLAG = 'A' ORDER BY RESERVE_RANK) WHERE ROWNUM = 1"
+                    CommandText = "SELECT * FROM (select A.JOB_NO JOB_NO, RESERVE_RANK, A.PLANT_CODE PLANT_CODE, A.ORDER_NO||' - '||A.ORDER_SEQ ORDER_NO, A.ORDER_M ORDER_M, A.ADD_GOOD_QTY ADD_GOOD_QTY, DECODE(A.GUBUN, 20, '내수', 40, '수출', 10, '특판') GUBUN, DECODE(A.WORK_GUBUN, 'A', '정상작업', 'U', '재작업', 'R', '연구개발', 'T', '기술테스트', 'S', 'S/BOOK', 'C', 'C/MATCH') WORK_GBN, DECODE(A.NOTE_FLAG, 'Y', '★') NOTE_FLAG, A.WORK_GUBUN WORK_GUBUN, A.WC_CODE WC_CODE, trim(A.PROD_CODE) PROD_CODE, PROD_NAME, A.PROD_UNIT PROD_UNIT, A.MRP_MGR MRP_MGR, NOTE0, NOTE1, TO_CHAR(START_DATE, 'HH24:MI') SDATE, TO_CHAR(END_DATE, 'HH24:MI') EDATE FROM TBL_PRODUCTPLAN A, TBL_PRODUCTMASTER B, TBL_PRODRESERVE C WHERE A.PROD_CODE  = B.PROD_CODE AND A.WC_CODE = C.WC_CODE AND A.JOB_NO = C.JOB_NO AND A.PLANT_CODE = '2020' AND A.PROC_STATUS = 'A' AND A.DEL_FLAG = 'A' ORDER BY RESERVE_RANK) WHERE ROWNUM = 1"
                 };
 
 
@@ -78,7 +64,6 @@ namespace FinalProject_Profile
                 reader.Read();
 
                 prod_code = reader["PROD_CODE"].ToString();
-                order_no = reader["ORDER_NO"].ToString();
                 job_no = reader["JOB_NO"].ToString();
                 prod_name = reader["PROD_NAME"].ToString();
                 prod_unit = reader["PROD_UNIT"].ToString();
@@ -104,16 +89,7 @@ namespace FinalProject_Profile
                 plt_box = Int32.Parse(reader["PLTBOX"].ToString());
                 cut_pcs = Int32.Parse(reader["CUTPCS"].ToString());
 
-                //생산테이블에서 데이터 조회 후 데이터를 컨트롤에 집어 넣는 작업
-                lbl_PROD_CODE.Text = prod_code;
-                lbl_ORDER_NO.Text = order_no;
-                lbl_PROD_NAME.Text = prod_name;
-                lbl_PROD_UNIT.Text = prod_unit;
-                lbl_ORDER_M.Text = order_m;
-                lbl_WORK_GBN.Text = work_gbn;
-                lbl_GUBUN.Text = gubun;
-
-
+                //todo 생산테이블에서 데이터 조회 후 데이터를 컨트롤에 집어 넣는 작업
 
                 //현재 진행중인 작업을 생산진행중 으로 바꾸는 쿼리
                 cmd = new OracleCommand
@@ -143,7 +119,7 @@ namespace FinalProject_Profile
         {
             System.Timers.Timer timer = new System.Timers.Timer
             {
-                Interval = 1000,
+                Interval = 100,
             };
             timer.Elapsed += new ElapsedEventHandler(timer_Elapsed);
             timer.Start();
@@ -165,7 +141,7 @@ namespace FinalProject_Profile
         }
         public void UpdateData()
         {
-            if (lbl_Prodution.InvokeRequired || lbl_BOX.InvokeRequired || lbl_PLT.InvokeRequired)
+            if (lbl_PCS.InvokeRequired || lbl_BOX.InvokeRequired || lbl_PLT.InvokeRequired)
             {
                 Invoke((MethodInvoker)delegate () {
                     UpdateData();
@@ -173,12 +149,12 @@ namespace FinalProject_Profile
             }
             else
             {
-                lbl_Prodution.Text = pcs.ToString();
+                lbl_PCS.Text = pcs.ToString();
                 lbl_BOX.Text = box.ToString();
                 lbl_PLT.Text = plt.ToString();
             }
 
-
+            
         }
     }
 }
